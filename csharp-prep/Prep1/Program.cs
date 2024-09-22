4f4fab76a104d6971f51b241733cd797e530eef9
Samuel <polarmi3@gmail.com>
2024-09-22 00:40:29 -0300

    first try

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask the last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Show the result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
