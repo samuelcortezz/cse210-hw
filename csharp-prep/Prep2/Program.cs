e8762aac790f29db2a3573c330ea9fc22e13f0ff
Samuel <polarmi3@gmail.com>
2024-09-23 11:51:12 -0300

    prep 2 program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Solicita el porcentaje de la calificación
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";  // La variable para almacenar la letra de la calificación
+
+        // Determina la letra de la calificación
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Mostrar la letra de la calificación
+        Console.WriteLine($"Your grade is: {letter}");
+
+        // Determinar si el estudiante pasó el curso
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying, you can do better next time.");
+        }
     }
 }
