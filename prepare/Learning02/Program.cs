413f479c7b582f0a36ec1336a1fbd4effc4f1378
Samuel <polarmi3@gmail.com>
2024-09-24 09:15:00 -0300

    customizing the code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,13 +8,13 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._startYear = 2018;
+        job1._endYear = 2021;
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
         job2._company = "Apple";
-        job2._startYear = 2022;
+        job2._startYear = 2021;
         job2._endYear = 2023;
 
         // Crear la instancia de Resume
